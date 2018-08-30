d8d650aa273887b58a0cf6d799891a7ecfd8730d
RuslanZinchenko <zinchenko.rusya@gmail.com>
2018-08-31 00:35:14 +0300

    module-02

--- JS/module-02/1/js/index.js
+++ JS/module-02/1/js/index.js
@@ -5,15 +5,19 @@ let numbers = [];
 let total = 0;
 
 do {
-  userInput = Number(prompt ('Введите число', ''));
-  numbers.push(userInput);
+  userInput = Number(prompt('Введите число', ''));
+  if (!isNaN(userInput)) {
+    numbers.push(userInput);
+  } else {
+    alert('Ошибка ввода!');
+  }
 } while (Number(userInput) && userInput !== null);
 
 console.log(numbers);
 
 for (let i of numbers) {
    total += i;
-};
+}
 
 console.log(total);
 alert(`Общая сумма чисел равна ${total}`);
