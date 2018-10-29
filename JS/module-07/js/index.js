47b5fb5877284a474d5f160bc0eaf1b65b58de30
RuslanZinchenko <zinchenko.rusya@gmail.com>
2018-10-30 00:08:35 +0200

    module-07

--- JS/module-07/js/index.js
+++ JS/module-07/js/index.js
@@ -1,59 +1,65 @@
-'use strict'
+"use strict";
 
 const posts = [
     {
-      img: "https://placeimg.com/400/150/arch",
-      title: "Post title 1",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
-      link: 'link-1.com'
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 1",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-1.com'
     },
     {
-      img: "https://placeimg.com/400/150/nature",
-      title: "Post title 2",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
-      link: 'link-2.com'
+        img: "https://placeimg.com/400/150/nature",
+        title: "Post title 2",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-2.com'
     },
     {
-      img: "https://placeimg.com/400/150/arch",
-      title: "Post title 3",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
-      link: 'link-3.com'
+        img: "https://placeimg.com/400/150/arch",
+        title: "Post title 3",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+        link: 'link-3.com'
     }
-  ];
+];
 
+const body = document.querySelector('body');
 
-for (let i of posts) {
-  // select element
-  let body = document.querySelector('body')
+const result = createCards (posts);
 
+body.append(...result); 
+
+/*Helpers*/
+
+function createCards (posts) {
+  return posts.reduce((acc, el) => acc.concat(createPostCard (el)), []);
+};
+
+function createPostCard ({img, title, text, link}) {
+  
   // create element
-  let post = document.createElement('div')
-  let img = document.createElement('img')
-  let h2 = document.createElement('h2')
-  let p = document.createElement('p')
-  let a = document.createElement('a')
+  const post = document.createElement('div');
+  const postImage = document.createElement('img');
+  const postTitle = document.createElement('h2');
+  const postText = document.createElement('p');
+  const button = document.createElement('a');
 
   // add class
-  post.classList.add('post')
-  img.classList.add('post__image')
-  h2.classList.add('post__title')
-  p.classList.add('post__text')
-  a.classList.add('button')
+  post.classList.add('post');
+  postImage.classList.add('post__image');
+  postTitle.classList.add('post__title');
+  postText.classList.add('post__text');
+  button.classList.add('button');
 
   // add attr
-  img.setAttribute('src', i.img)
-  img.setAttribute('alt', 'post image')
-  a.setAttribute('href', '#')
+  postImage.setAttribute('src', img);
+  postImage.setAttribute('alt', 'post image');
+  button.setAttribute('href', '#');
 
   // paste textContent
-  h2.textContent = i.title;
-  p.textContent =i.text;
-  a.textContent = i.link;
+  postTitle.textContent = title;
+  postText.textContent = text;
+  button.textContent = link;
 
-  // paste el on screen
-  body.append(post)
-  post.append(img)
-  post.append(h2)
-  post.append(p)
-  post.append(a)
+  post.append(postImage, postTitle, postText, button);
+
+  return post;
 }
