7cc73e405620f2e8d785a87dd38aca4eccdefd0a
RuslanZinchenko <zinchenko.rusya@gmail.com>
2019-04-24 03:10:24 +0300

    Updates

---
+++ precache-manifest.820814265b141951c945789c1497aebd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3116841df23c337070e",
+    "url": "/react-group-5/static/js/runtime~main.db387c26.js"
+  },
+  {
+    "revision": "4c5cdca024731d81931b",
+    "url": "/react-group-5/static/js/main.05de48a2.chunk.js"
+  },
+  {
+    "revision": "c31b5ecc3c8d85e17e7d",
+    "url": "/react-group-5/static/js/2.44e0a5b2.chunk.js"
+  },
+  {
+    "revision": "4c5cdca024731d81931b",
+    "url": "/react-group-5/static/css/main.2391d35e.chunk.css"
+  },
+  {
+    "revision": "014955e2d56681ffc688425ae559b842",
+    "url": "/react-group-5/index.html"
+  }
+];
